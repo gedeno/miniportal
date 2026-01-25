02d08a0e048b3076d18d0fb9b83443192e4ae086
gedeno <gedenogeremew@gmail.com>
2026-01-25 22:43:05 +0300

    new models are created

--- main/models.py
+++ main/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class studs(models.model):
+    name = models.CharField(max_length=100)
+    id_no = models.CharField(max_length=20)
+    age = models.IntegerField()
+    def __str__(self):
+        return self.name
+class grade(models.Model):
+    quiz1 = models.FloatField()
+    quiz2 = models.FloatField()
+    assignment = models.FloatField()
+    assignment2 = models.FloatField()
+    mid_exam = models.FloatField()
+    final_exam = models.FloatField()
+    student = models.ForeignKey(studs, on_delete=models.CASCADE)
+    def __str__(self):  
+        return f"Grades of {self.student.name}"
