e4396640c5b0382c32e47dbe8f2c14a51fae5b58
gedeno <gedenogeremew@gmail.com>
2026-03-23 22:47:05 +0300

    small edition

--- main/urls.py
+++ main/urls.py
@@ -3,8 +3,11 @@ from .  import views
 
 urlpatterns = [
     path('home/', views.home, ),
+
     path('teach/', views.teach),
+
     path('subject/<int:id>/',views.Subject),
+    
     path('teach2/<int:id>/',views.teach2),
     path('disp/',views.display),
     path('subjects1/<int:id>/',views.subjects1),
